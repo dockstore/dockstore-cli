62011f1d34a015cf8a8a49fe1139d2d4fac8cba1
Denis Yuen <denis.yuen@oicr.on.ca>
2019-09-11 17:44:21 -0400

    update secret archive

--- encrypt.sh
+++ encrypt.sh
@@ -6,7 +6,7 @@ set -o xtrace
 
 regex="encrypted_([0-9a-zA-Z]+)_key"
 
-tar cvf secrets.tar dockstore-integration-testing/src/test/resources/dstesting_pcks8.pem dockstore-integration-testing/src/test/resources/config_file.txt dockstore-integration-testing/src/test/resources/config_file2.txt dockstore-webservice/src/main/resources/migrations.test.confidential2.xml dockstore-integration-testing/src/test/resources/dockstoreTest.yml dockstore-webservice/src/main/resources/migrations.test.confidential1.xml dockstore-webservice/src/main/resources/migrations.test.confidential1_1.5.0.xml dockstore-webservice/src/main/resources/migrations.test.confidential2_1.5.0.xml
+tar cvf secrets.tar dockstore-cli-integration-testing/src/test/resources/dstesting_pcks8.pem dockstore-cli-integration-testing/src/test/resources/config_file.txt dockstore-cli-integration-testing/src/test/resources/config_file2.txt dockstore-webservice/src/main/resources/migrations.test.confidential2.xml dockstore-cli-integration-testing/src/test/resources/dockstoreTest.yml dockstore-webservice/src/main/resources/migrations.test.confidential1.xml dockstore-webservice/src/main/resources/migrations.test.confidential1_1.5.0.xml dockstore-webservice/src/main/resources/migrations.test.confidential2_1.5.0.xml
 
 # store working dir
 GIT_DIR=`pwd`
