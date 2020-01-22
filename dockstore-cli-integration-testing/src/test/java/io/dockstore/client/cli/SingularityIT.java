5ce110fca1738aebdd15bdd06f1dd06c660c9ff8
Emily Soth <esoth@ucsc.edu>
2020-01-22 12:32:24 -0800

    testing making a new test category

---
+++ dockstore-cli-integration-testing/src/test/java/io/dockstore/client/cli/SingularityIT.java
@@ -0,0 +1,23 @@
+package io.dockstore.client.cli;
+
+import io.dockstore.common.SingularityTest;
+import io.dropwizard.testing.ResourceHelpers;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(SingularityTest.class)
+public class SingularityIT extends BaseIT {
+
+    @Test
+    public void runCwlWorkflow() {
+        Client.main(new String[] {
+            "--config",
+            ResourceHelpers.resourceFilePath("config_for_singularity"),
+            "workflow",
+            "launch",
+            "--entry",
+            "github.com/dockstore-testing/md5sum-checker/_cwl_checker:1.0.1",
+            "--json", ResourceHelpers.resourceFilePath("md5sum-wrapper-tool.json")
+        });
+    }
+}
