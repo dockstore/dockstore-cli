6f719c0a8786fe6ac2aa180b531d20e5cbd39889
Emily Soth <esoth@ucsc.edu>
2020-01-22 14:40:25 -0800

    added decrypt for singularity tests

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -8,7 +8,7 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-if [[ "${TESTING_PROFILE}" == "confidential"* ]] || [[ "${TESTING_PROFILE}" == "automated-review" ]]; then
+if [[ "${TESTING_PROFILE}" == "confidential"* ]] || [[ "${TESTING_PROFILE}" == "automated-review" ]] || [[ "${TESTING_PROFILE}" == "singularity"* ]]; then
     openssl aes-256-cbc -K $encrypted_04282e75bce6_key -iv $encrypted_04282e75bce6_iv -in secrets.tar.enc -out secrets.tar -d
     tar xvf secrets.tar
     mv dockstore-cli-integration-testing/src/test/resources/dstesting_pcks8.pem /home/travis/dstesting_pcks8.pem
