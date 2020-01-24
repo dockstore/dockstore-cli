762127409cd5c4936af4285d373f11aba34aa67a
Emily Soth <esoth@ucsc.edu>
2020-01-24 10:11:08 -0800

    switched to locally hosted version of workflow

--- dockstore-cli-integration-testing/src/test/java/io/dockstore/client/cli/SingularityIT.java
+++ dockstore-cli-integration-testing/src/test/java/io/dockstore/client/cli/SingularityIT.java
@@ -16,7 +16,7 @@ public class SingularityIT extends BaseIT {
             "workflow",
             "launch",
             "--entry",
-            "github.com/dockstore-testing/md5sum-checker/_cwl_checker:1.0.1",
+            "github.com/DockstoreTestUser2/md5sum-checker/testname",
             "--json", ResourceHelpers.resourceFilePath("md5sum-wrapper-tool.json")
         });
     }
