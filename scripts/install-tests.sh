2af6afac504f7704995fffe17d204d86095e7ec3
Emily Soth <esoth@ucsc.edu>
2020-01-24 09:55:32 -0800

    more sudo

--- scripts/install-tests.sh
+++ scripts/install-tests.sh
@@ -23,7 +23,7 @@ if [ "${TESTING_PROFILE}" = "singularity-tests" ]; then
     wget -O- http://neuro.debian.net/lists/xenial.us-ca.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list && \
     apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9 && \
     apt-get update
-    apt-get install -y singularity-container
+    sudo apt-get install -y singularity-container
 fi
 
 # hook up integration tests with elastic search
