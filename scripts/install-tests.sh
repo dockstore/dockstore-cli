88e8c9f812bda2bbfb89e52aca3ad02c7921abb4
Emily Soth <esoth@ucsc.edu>
2020-02-10 12:11:04 -0800

    moved singularity install to its own script

--- scripts/install-tests.sh
+++ scripts/install-tests.sh
@@ -19,41 +19,6 @@ else
     pip3 install --user -r https://raw.githubusercontent.com/dockstore/dockstore/develop/dockstore-webservice/src/main/resources/requirements/1.7.0/requirements3.txt
 fi
 
-if [[ "${TESTING_PROFILE}" = "singularity-tests" ]] && [[ ! -d $HOME/.builddir ]]; then
-    # Install singularity from source
-    # need singularity > 3.0.0, which is not available as an ubuntu package
-    sudo apt-get update && sudo apt-get install -y \
-    build-essential \
-    libssl-dev \
-    uuid-dev \
-    libgpgme11-dev \
-    squashfs-tools \
-    libseccomp-dev \
-    pkg-config
-
-    # Install Go (needed to install singularity)
-    export VERSION=1.11 OS=linux ARCH=amd64 && \
-    wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
-    sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
-    rm go$VERSION.$OS-$ARCH.tar.gz
-
-    # Download and install singularity from a release
-    export VERSION=3.0.3 && # adjust this as necessary \
-    mkdir -p $GOPATH/src/github.com/sylabs && \
-    cd $GOPATH/src/github.com/sylabs && \
-    wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
-    tar -xzf singularity-${VERSION}.tar.gz && \
-    cd ./singularity && \
-    ./mconfig
-
-    # Compile singularity
-    ./mconfig && \
-    make -C ./builddir && \
-    sudo make -C ./builddir install
-
-    singularity --version
-fi
-
 # hook up integration tests with elastic search
 docker pull elasticsearch:5.6.3
 docker run -p 9200:9200 -d elasticsearch:5.6.3
