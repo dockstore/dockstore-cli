cbe3b0e4a943bb751e628de825535bdc690a1812
Emily Soth <esoth@ucsc.edu>
2020-02-10 10:14:21 -0800

    pr updates; attempting caching

--- dockstore-cli-integration-testing/src/test/java/io/dockstore/client/cli/SingularityIT.java
+++ dockstore-cli-integration-testing/src/test/java/io/dockstore/client/cli/SingularityIT.java
@@ -1,9 +1,7 @@
 package io.dockstore.client.cli;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 
 import io.dockstore.client.cli.nested.SingularityTest;
@@ -14,11 +12,12 @@ import io.swagger.client.ApiClient;
 import io.swagger.client.api.WorkflowsApi;
 import io.swagger.client.model.Workflow;
 import org.apache.commons.io.FileUtils;
-import org.bouncycastle.util.io.TeeOutputStream;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.experimental.categories.Category;
 import org.junit.rules.TemporaryFolder;
 
@@ -28,7 +27,10 @@ public class SingularityIT extends BaseIT {
     @ClassRule
     public static TemporaryFolder temporaryFolder = new TemporaryFolder();
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private static final String CONFIG = ResourceHelpers.resourceFilePath("config_for_singularity");
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 
     @Before
     @Override
@@ -49,33 +51,26 @@ public class SingularityIT extends BaseIT {
                 "/checker-workflow-wrapping-workflow.cwl", "test", "cwl", null);
         workflowApi.refresh(workflow.getId());
 
-        // start saving the output so as to make sure that Singularity actually runs instead of Docker
-        PrintStream old = System.out;  // save the regular output stream so it can be reset later
-        TeeOutputStream teeOut = new TeeOutputStream(System.out, outContent);
-        PrintStream out = new PrintStream(teeOut, true);  // also print it to the screen
-        System.setOut(out);
-
         // run the md5sum-checker workflow
         Client.main(new String[] {
             "--config",  // this config file passes the --singularity option to cwltool
-            ResourceHelpers.resourceFilePath("config_for_singularity"),
+            CONFIG,
             "workflow",
             "launch",
             "--entry",
             SourceControl.GITHUB.toString() + "/DockstoreTestUser2/md5sum-checker/test",
             "--json", ResourceHelpers.resourceFilePath("md5sum_cwl.json")
         });
-        System.setOut(old);  // reset the output stream to stdout
 
         // the message "Creating SIF file" will only be in the output if the Singularity command starts successfully
-        Assert.assertTrue(outContent.toString().contains("Creating SIF file"));
+        Assert.assertTrue("assert output contains singularity command", systemOutRule.getLog().contains("Creating SIF file"));
     }
 
     /**
      * Tests that a simple WDL workflow can be run in Singularity instead of Docker
      */
     @Test
-    public void runWDLWorkflow() {
+    public void runWDLWorkflow() throws IOException {
         // manually register the WDL version of the md5sum-checker workflow locally
         testingPostgres.runUpdateStatement("update enduser set isadmin = 't' where username = 'DockstoreTestUser2';");
         final ApiClient webClient = getWebClient(USER_2_USERNAME, testingPostgres);
@@ -84,14 +79,9 @@ public class SingularityIT extends BaseIT {
                 "/checker-workflow-wrapping-workflow.wdl", "test", "wdl", null);
         workflowApi.refresh(workflow.getId());
 
-        // make a tmp copy of the dockstore config and add to it the cromwell conf file option
-        File tmpConfig = generateCromwellConfig();  // this is done in the test because the location varies
-
-        // start saving the output so as to make sure that Singularity actually runs instead of Docker
-        PrintStream old = System.out;  // save the regular output stream so it can be reset later
-        TeeOutputStream teeOut = new TeeOutputStream(System.out, outContent);
-        PrintStream out = new PrintStream(teeOut, true);  // also print it to the screen
-        System.setOut(out);
+        // make a tmp copy of the dockstore config and add the cromwell conf file option to it
+        // this is done in the test because the path to the cromwell conf is different if it's running locally vs. on Travis
+        File tmpConfig = generateCromwellConfig();
 
         // run the md5sum-checker workflow
         Client.main(new String[] {
@@ -103,38 +93,32 @@ public class SingularityIT extends BaseIT {
             SourceControl.GITHUB.toString() + "/DockstoreTestUser2/md5sum-checker/test",
             "--json", ResourceHelpers.resourceFilePath("md5sum_wdl.json")
         });
-        System.setOut(old);  // reset the output stream to stdout
 
         // the phrase "singularity exec" will only be in the output if Singularity is actually being used
-        Assert.assertTrue(outContent.toString().contains("singularity exec"));
+        Assert.assertTrue("assert output contains singularity command", systemOutRule.getLog().contains("singularity exec"));
     }
 
     /**
      * Makes a tmp file that's a copy of the contents of the config_for_singularity file in resources,
-     * and adds a line setting the cromwell-vm-options to the cromwell_singularity.conf path
+     * and adds a line setting the cromwell-vm-options to the cromwell_singularity.conf path.
+     * This is needed to get the absolute path of the file, which is different if it's being run locally or on Travis.
      * @return File object of the tmp file created
      */
-    private File generateCromwellConfig() {
+    private File generateCromwellConfig() throws IOException {
         // get the path to the cromwell conf file in the current file system
         // this configures cromwell to run with singularity instead of docker
         String cromwellConfPath = ResourceHelpers.resourceFilePath("cromwell_singularity.conf");
 
-        try {
-            // make a new file in the tmp folder for this test
-            File tmpFile = temporaryFolder.newFile("config_for_singularity");
-            // copy the dockstore config for singularity from the resources directory into the new tmp file
-            File configTemplate = new File(ResourceHelpers.resourceFilePath("config_for_singularity"));
-            FileUtils.copyFile(configTemplate, tmpFile);
+        // make a new file in the tmp folder for this test
+        File tmpFile = temporaryFolder.newFile("config_for_singularity");
+        // copy the dockstore config for singularity from the resources directory into the new tmp file
+        File configTemplate = new File(CONFIG);
+        FileUtils.copyFile(configTemplate, tmpFile);
 
-            // append the new cromwell option to the tmp dockstore config
-            FileUtils.writeStringToFile(tmpFile, "\ncromwell-vm-options: -Dconfig.file=" + cromwellConfPath,
-                    StandardCharsets.UTF_8, true);
-            return tmpFile;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+        // append the new cromwell option to the tmp dockstore config
+        FileUtils.writeStringToFile(tmpFile, "\ncromwell-vm-options: -Dconfig.file=" + cromwellConfPath,
+                StandardCharsets.UTF_8, true);
+        return tmpFile;
     }
 }
 
