59b022830525c6b08ac81f79d8bf3077dd9cd10d
Emily Soth <esoth@ucsc.edu>
2020-02-19 10:20:52 -0800

    move singularity install script into install-tests

--- scripts/install-tests.sh
+++ scripts/install-tests.sh
@@ -20,7 +20,37 @@ else
 fi
 
 if [ "${TESTING_PROFILE}" = "singularity-tests" ]; then
-    ./install-singularity.sh
+    # Install singularity from source
+    # need singularity > 3.0.0, which is not available as an ubuntu package
+    sudo apt-get update && sudo apt-get install -y \
+    build-essential \
+    libssl-dev \
+    uuid-dev \
+    libgpgme11-dev \
+    squashfs-tools \
+    libseccomp-dev \
+    pkg-config
+
+    # Install Go (needed to install singularity)
+    export VERSION=1.11 OS=linux ARCH=amd64 && \
+    wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
+    sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
+    rm go$VERSION.$OS-$ARCH.tar.gz
+
+    # Download and install singularity from a release
+    export VERSION=3.0.3 && # adjust this as necessary \
+    mkdir -p "$GOPATH"/src/github.com/sylabs && \
+    cd "$GOPATH"/src/github.com/sylabs && \
+    wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
+    tar -xzf singularity-${VERSION}.tar.gz && \
+    cd ./singularity && \
+    ./mconfig
+
+    # Compile singularity
+    ./mconfig && \
+    make -C ./builddir && \
+    sudo make -C ./builddir install
+    singularity --version
 fi
 
 # hook up integration tests with elastic search
