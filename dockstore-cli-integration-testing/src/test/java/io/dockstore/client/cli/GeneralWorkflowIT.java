2a6385b9a47792b5420c4296df4feec028488b14
Gary Luu <garylau.work@gmail.com>
2020-06-02 14:20:19 -0400

    Test hotfix (#27)
    * Use 1.8.6 webservice
    
    * Update .travis.yml
    
    * Fix some tests
    
    * Disable GitLab test, enable for 1.9.0

--- dockstore-cli-integration-testing/src/test/java/io/dockstore/client/cli/GeneralWorkflowIT.java
+++ dockstore-cli-integration-testing/src/test/java/io/dockstore/client/cli/GeneralWorkflowIT.java
@@ -697,7 +697,8 @@ public class GeneralWorkflowIT extends BaseIT {
      */
     @Ignore
     @Test
-    public void testGitlab() {
+    @Ignore("Ignoring for 1.8.6, enable for 1.9.0")
+    public void testGitLab() {
         // Refresh workflow
         Client.main(new String[] { "--config", ResourceHelpers.resourceFilePath("config_file2.txt"), "workflow", "refresh", "--script" });
         Client.main(new String[] { "--config", ResourceHelpers.resourceFilePath("config_file2.txt"), "workflow", "refresh", "--entry",
@@ -815,6 +816,7 @@ public class GeneralWorkflowIT extends BaseIT {
      */
     @Ignore
     @Test
+    @Ignore("Ignoring for 1.8.6, enable for 1.9.0")
     public void testManualPublishGitlab() {
         // manual publish
         Client.main(
