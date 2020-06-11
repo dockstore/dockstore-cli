3a0271cd19fbb1068b8751be972082fb6d65ff31
Charles Overbeck <coverbec@ucsc.edu>
2020-06-11 09:55:58 -0700

    Theory as to why tests aren't passing on Travis.

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -8,7 +8,7 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-if [[ "${TESTING_PROFILE}" == *"integration-tests"* ]] || [[ "${TESTING_PROFILE}" == "automated-review" ]]; then
+if [[ "${TESTING_PROFILE}" == "confidential-workflow-tests" ]] || [[ "${TESTING_PROFILE}" == "confidential-tool-tests" ]] || [[ "${TESTING_PROFILE}" == "automated-review" ]]; then
     openssl aes-256-cbc -K $encrypted_292dafccc281_key -iv $encrypted_292dafccc281_iv -in secrets.tar.enc -out secrets.tar -d
     tar xvf secrets.tar
     mv dockstore-cli-integration-testing/src/test/resources/dstesting_pcks8.pem /home/travis/dstesting_pcks8.pem
