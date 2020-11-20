44e11d90e5ef984d89cb15027eb0db355cf729c1
Walt Shands <jshands@ucsc.edu>
2020-11-20 10:55:30 -0800

    fix decrypt script and requirements version

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -8,7 +8,7 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-if [[ "${TESTING_PROFILE}" =~ ^(confidential-workflow-tests|confidential-tool-tests|automated-review|singularity-tests)$ ]]; then
+if [[ "${TESTING_PROFILE}" =~ ^(regression-integration-tests|confidential-workflow-tests|confidential-tool-tests|automated-review|singularity-tests)$ ]]; then
     openssl aes-256-cbc -K $encrypted_292dafccc281_key -iv $encrypted_292dafccc281_iv -in secrets.tar.enc -out secrets.tar -d
     tar xvf secrets.tar
     mv dockstore-cli-integration-testing/src/test/resources/dstesting_pcks8.pem /home/travis/dstesting_pcks8.pem
