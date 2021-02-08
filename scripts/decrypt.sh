8df2d2502efc69858b394629b7e9548029600702
Walt Shands <jshands@ucsc.edu>
2021-02-08 14:27:13 -0800

    refactor circleCI config

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -13,6 +13,10 @@ set -o xtrace
 
 openssl aes-256-cbc -d -in circle_ci_test_data.zip.enc -k "$CIRCLE_CI_KEY" -iv "$CIRCLE_CI_IV" -out secrets.tar
 tar xvf secrets.tar
+# Create the directory where the .pem file will be placed for continuous integration
+# If you would like to change this path (might be a good idea since we no longer use
+# TravisCI for CI) you need to change the path in dockstoreTest.yml which currently
+# is in the encrypted bundle circle_ci_test_data.zip.enc
 sudo mkdir -p /home/travis
 sudo cp dockstore-cli-integration-testing/src/test/resources/dstesting_pcks8.pem /home/travis/dstesting_pcks8.pem
 rm secrets.tar
