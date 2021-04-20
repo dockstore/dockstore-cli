7917c41b8af5ade112ca7badbb715115d0f7111f
Charles Overbeck <coverbec@ucsc.edu>
2021-04-20 14:56:04 -0700

    Rotate keys and reencrypt bundle (#4203)
    New variables because I don't want to break existing branches

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # This script decrypts our test database
 # Bash3 Boilerplate. Copyright (c) 2014, kvz.io
-# You will need the environment variables CIRCLE_CI_KEY and CIRCLE_CI_IV
+# You will need the environment variables CIRCLE_CI_KEY_2 and CIRCLE_CI_IV_2
 
 set -o errexit
 set -o pipefail
 set -o nounset
 set -o xtrace
 
-: "$CIRCLE_CI_KEY"
-: "$CIRCLE_CI_IV"
+: "$CIRCLE_CI_KEY_2"
+: "$CIRCLE_CI_IV_2"
 
-openssl aes-256-cbc -d -in circle_ci_test_data.zip.enc -k "$CIRCLE_CI_KEY" -iv "$CIRCLE_CI_IV" -out secrets.tar
+openssl aes-256-cbc -d -in circle_ci_test_data.zip.enc -k "$CIRCLE_CI_KEY_2" -iv "$CIRCLE_CI_IV_2" -out secrets.tar
 tar xvf secrets.tar
 # Create the directory where the .pem file will be placed for continuous integration
 # If you would like to change this path (might be a good idea since we no longer use
