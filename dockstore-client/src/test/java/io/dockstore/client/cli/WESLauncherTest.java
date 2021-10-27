eb0113c1cf8db2f32f6d30f3f295c5bb65e3430e
richard-hansen <richie.hansen@gmail.com>
2021-10-27 11:54:46 -0400

    Checkstyle fixes

--- dockstore-client/src/test/java/io/dockstore/client/cli/WESLauncherTest.java
+++ dockstore-client/src/test/java/io/dockstore/client/cli/WESLauncherTest.java
@@ -1,8 +1,5 @@
 package io.dockstore.client.cli;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import io.dockstore.client.cli.nested.AbstractEntryClient;
 import io.dockstore.client.cli.nested.WESLauncher;
 import io.dockstore.client.cli.nested.WesRequestData;
@@ -12,10 +9,8 @@ import io.dropwizard.testing.ResourceHelpers;
 import io.swagger.client.api.UsersApi;
 import io.swagger.client.api.WorkflowsApi;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
 public class WESLauncherTest {
