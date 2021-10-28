66523fbe0ffc6e33eeb4a699f7ad0acea1e9bf11
richard-hansen <richie.hansen@gmail.com>
2021-10-28 15:38:45 -0400

    Test improvements

--- dockstore-client/src/test/java/io/dockstore/client/cli/WESLauncherTest.java
+++ dockstore-client/src/test/java/io/dockstore/client/cli/WESLauncherTest.java
@@ -10,6 +10,7 @@ import io.swagger.client.api.UsersApi;
 import io.swagger.client.api.WorkflowsApi;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
@@ -41,12 +42,20 @@ public class WESLauncherTest {
         final String basePath = "https://dockstore.org";
         final String entryId = "github.com/org/repo";
         final String versionId = "master";
-        final String type = "WDL";
+        final String type = "PLAIN_WDL";
         final String relativePath = "rightHere.wdl";
         final String trsUrl = launcher.createTrsUrl(basePath, entryId, versionId, type, relativePath);
 
+        final String expectedResult = "https://dockstore.org/ga4gh/trs/v2/tools/github.com%2Forg%2Frepo/versions/master/PLAIN_WDL/descriptor/rightHere.wdl";
+        assertEquals("Checking the entire URL", expectedResult, trsUrl);
         assertTrue("The basepath shouldn't be encoded", trsUrl.contains(basePath));
         assertTrue("workflow path should be encoded", trsUrl.contains("github.com%2Forg%2Frepo"));
+
+        // Ensure the #workflow prefix is correctly encoded
+        final String trsWorkflowUrl = launcher.createTrsUrl(basePath, "#workflow/"+entryId, versionId, type, relativePath);
+
+        final String expectedWorkflowResult = "https://dockstore.org/ga4gh/trs/v2/tools/%23workflow%2Fgithub.com%2Forg%2Frepo/versions/master/PLAIN_WDL/descriptor/rightHere.wdl";
+        assertEquals("Checking the entire URL", expectedWorkflowResult, trsWorkflowUrl);
 
     }
 }
