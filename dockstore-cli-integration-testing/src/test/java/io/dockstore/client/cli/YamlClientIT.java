a08c280d7d4c8bb52f408d4b7d401103f2f031b9
Flynn Hembroff <flynnhembroff@gmail.com>
2022-07-07 11:55:21 -0400

    Corrected tests in YamlClientIT and included "--config", TestUtility.getConfigFileLocation(true) in each call to Client.main

--- dockstore-cli-integration-testing/src/test/java/io/dockstore/client/cli/YamlClientIT.java
+++ dockstore-cli-integration-testing/src/test/java/io/dockstore/client/cli/YamlClientIT.java
@@ -1,13 +1,13 @@
 package io.dockstore.client.cli;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
-import com.google.common.collect.Lists;
+import io.dockstore.common.CommonTestUtilities;
 import io.dockstore.common.FlushingSystemErrRule;
 import io.dockstore.common.FlushingSystemOutRule;
 import io.dockstore.common.TestUtility;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.ExpectedSystemExit;
@@ -27,45 +27,41 @@ public class YamlClientIT extends BaseIT {
     @Rule
     public final ExpectedSystemExit systemExit = ExpectedSystemExit.none();
 
-    @Test
-    public void testAllYamlHelpMessages() throws IOException {
-        checkCommandForHelp(new String[] { "yaml"});
-        checkCommandForHelp(new String[] { "yaml", "--help" });
-        checkCommandForHelp(new String[] { "yaml", "validate", "--help" });
+    @Before
+    @Override
+    public void resetDBBetweenTests() throws Exception {
+        CommonTestUtilities.cleanStatePrivate1(SUPPORT);
+        Client.DEBUG.set(false);
     }
 
-    private void checkCommandForHelp(String[] argv) throws IOException {
-        final ArrayList<String> strings = Lists.newArrayList(argv);
-        strings.add("--config");
-        strings.add(TestUtility.getConfigFileLocation(true));
 
-        Client.main(strings.toArray(new String[0]));
-        System.out.println(strings.toArray(new String[0]));
-        Assert.assertTrue(systemOutRule.getLog().contains("Usage: dockstore"));
-        systemOutRule.clearLog();
-    }
 
     @Test
-    public void missingPathParameter() {
-        Client.main(new String[] { "yaml", "validate" });
+    public void missingPathParameter() throws IOException {
+        Client.main(new String[] { "--config", TestUtility.getConfigFileLocation(true), "yaml", "validate" });
         Assert.assertTrue(systemOutRule.getLog().contains("ERROR: Missing --path <path>"));
         systemOutRule.clearLog();
     }
 
     @Test
-    public void completeRun() {
-        Client.main(new String[] { "yaml", "validate", "--path", "../dockstore-client/src/test/resources/YamlVerifyTestDirectory/correct-directory" });
+    public void completeRun() throws IOException {
+        Client.main(new String[] { "--config", TestUtility.getConfigFileLocation(true), "yaml", "validate", "--path", "../dockstore-client/src/test/resources/YamlVerifyTestDirectory/correct-directory" });
         assertTrue(systemOutRule.getLog().contains("src/test/resources/YamlVerifyTestDirectory/correct-directory/.dockstore.yml is a valid yaml file"));
         assertTrue(systemOutRule.getLog().contains("src/test/resources/YamlVerifyTestDirectory/correct-directory/.dockstore.yml is a valid dockstore yaml file"));
+        systemOutRule.clearLog();
     }
 
     @Test
-    public void verifyErrorMessagesArePrinted() {
-        Client.main(new String[] { "yaml", "validate", "--path", "../dockstore-client/src/test/resources/YamlVerifyTestDirectory/correct-directory" });
-        String errorMsg = "../dockstore-client/src/test/resources/YamlVerifyTestDirectory/correct-directory/.dockstore.yml is a valid yaml file\n"
-            + "../dockstore-client/src/test/resources/YamlVerifyTestDirectory/correct-directory/.dockstore.yml is a valid dockstore yaml file\n";
-        Assert.assertEquals(errorMsg, systemOutRule.getLog());
+    public void verifyErrorMessagesArePrinted() throws IOException {
+        Client.main(new String[] { "--config", TestUtility.getConfigFileLocation(true), "yaml", "validate", "--path", "../dockstore-client/src/test/resources/YamlVerifyTestDirectory/some-files-present" });
+        assertTrue(systemOutRule.getLog().contains("../dockstore-client/src/test/resources/YamlVerifyTestDirectory/some-files-present/.dockstore.yml is a valid yaml file"));
+        String errorMsg = "Your file structure has the following errors:\n"
+            + "../dockstore-client/src/test/resources/YamlVerifyTestDirectory/some-files-present/dockstore.wdl.json does not exist\n"
+            + "../dockstore-client/src/test/resources/YamlVerifyTestDirectory/some-files-present/Dockstore.cwl does not exist";
+        assertTrue(systemOutRule.getLog().contains(errorMsg));
+        systemOutRule.clearLog();
     }
+
 
 
 }
