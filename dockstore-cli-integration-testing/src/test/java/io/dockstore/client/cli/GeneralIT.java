0ab17529a8c259cf56564d7c055b2727a7774033
Denis Yuen <denis.yuen@oicr.on.ca>
2022-07-13 16:22:05 -0400

    less exciting theory

--- dockstore-cli-integration-testing/src/test/java/io/dockstore/client/cli/GeneralIT.java
+++ dockstore-cli-integration-testing/src/test/java/io/dockstore/client/cli/GeneralIT.java
@@ -316,6 +316,9 @@ public class GeneralIT extends BaseIT {
     public void testTool2JSONWDL() {
         Client.main(new String[] { "--config", ResourceHelpers.resourceFilePath("config_file2.txt"), "tool", "publish", "--entry",
             "quay.io/dockstoretestuser2/quayandgithubwdl" });
+        // need to refresh to overrride bad data in ye-olde seed DB that trips up improved validation now
+        Client.main(new String[] { "--config", ResourceHelpers.resourceFilePath("config_file2.txt"), "tool", "refresh", "--entry",
+            "quay.io/dockstoretestuser2/quayandgithubwdl", "--script" });
         // need to publish before converting
         Client.main(
             new String[] { "--config", ResourceHelpers.resourceFilePath("config_file2.txt"), "tool", "convert", "entry2json", "--entry",
