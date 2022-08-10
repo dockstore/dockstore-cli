c81958e7b8bddcc534fa2e921438e7201016a945
Flynn Hembroff <flynnhembroff@gmail.com>
2022-08-10 12:10:21 -0400

    Removed unnecessary throw statement.

--- dockstore-cli-integration-testing/src/test/java/io/dockstore/common/CommonTestUtilities.java
+++ dockstore-cli-integration-testing/src/test/java/io/dockstore/common/CommonTestUtilities.java
@@ -156,7 +156,7 @@ public final class CommonTestUtilities {
      * @param testingPostgres reference to the testing instance of Postgres
      * @throws Exception
      */
-    public static void deleteBitBucketToken(TestingPostgres testingPostgres) throws Exception {
+    public static void deleteBitBucketToken(TestingPostgres testingPostgres)  {
         LOG.info("Deleting BitBucket Token from Database");
         testingPostgres.runUpdateStatement("delete from token where tokensource = 'bitbucket.org'");
     }
