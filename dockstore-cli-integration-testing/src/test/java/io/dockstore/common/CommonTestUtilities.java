8e24e46d37c5812bdc08d71b24866b2999ecc58b
Denis Yuen <denis.yuen@oicr.on.ca>
2022-12-14 17:23:27 -0500

    Need cli equivalent of https://github.com/dockstore/dockstore/pull/5243

--- dockstore-cli-integration-testing/src/test/java/io/dockstore/common/CommonTestUtilities.java
+++ dockstore-cli-integration-testing/src/test/java/io/dockstore/common/CommonTestUtilities.java
@@ -83,7 +83,7 @@ public final class CommonTestUtilities {
         application.run("db", "drop-all", "--confirm-delete-everything", dropwizardConfigurationFile);
         application
             .run("db", "migrate", dropwizardConfigurationFile, "--include", "1.3.0.generated,1.3.1.consistency,1.4.0,1.5.0,"
-                    + "1.6.0,1.7.0,1.8.0,1.9.0,1.10.0,1.11.0,1.12.0,1.13.0");
+                    + "1.6.0,1.7.0,1.8.0,1.9.0,1.10.0,1.11.0,1.12.0,1.13.0,1.14.0");
     }
 
     /**
@@ -110,7 +110,7 @@ public final class CommonTestUtilities {
 
         List<String> migrationList = Arrays
             .asList("1.3.0.generated", "1.3.1.consistency", "test", "1.4.0", "1.5.0", "test_1.5.0", "1.6.0", "1.7.0",
-                    "1.8.0", "1.9.0", "1.10.0", "1.11.0", "1.12.0", "1.13.0");
+                    "1.8.0", "1.9.0", "1.10.0", "1.11.0", "1.12.0", "1.13.0", "1.14.0");
         runMigration(migrationList, application, dropwizardConfigurationFile);
     }
 
@@ -214,7 +214,7 @@ public final class CommonTestUtilities {
                 new File("../dockstore-webservice/src/main/resources/migrations.test.confidential1_1.5.0.xml").getAbsolutePath());
         runExternalMigration(migrationList, application, configPath);
 
-        migrationList = Arrays.asList("1.6.0", "1.7.0", "1.8.0", "1.9.0", "1.10.0", "1.11.0", "1.12.0", "1.13.0");
+        migrationList = Arrays.asList("1.6.0", "1.7.0", "1.8.0", "1.9.0", "1.10.0", "1.11.0", "1.12.0", "1.13.0", "1.14.0");
         runMigration(migrationList, application, configPath);
     }
 
@@ -299,7 +299,7 @@ public final class CommonTestUtilities {
                 new File("../dockstore-webservice/src/main/resources/migrations.test.confidential2_1.5.0.xml").getAbsolutePath());
         runExternalMigration(migrationList, application, configPath);
 
-        migrationList = Arrays.asList("1.6.0", "1.7.0", "1.8.0", "1.9.0", "1.10.0", "1.11.0", "1.12.0", "1.13.0");
+        migrationList = Arrays.asList("1.6.0", "1.7.0", "1.8.0", "1.9.0", "1.10.0", "1.11.0", "1.12.0", "1.13.0", "1.14.0");
         runMigration(migrationList, application, configPath);
     }
 
