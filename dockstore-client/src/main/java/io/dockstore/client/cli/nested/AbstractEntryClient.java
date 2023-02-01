409f00e6c818a6d1d9330af7bf19944b47404296
fhembroff <flynnhembroff@gmail.com>
2023-02-01 10:31:27 -0500

    Corrected typo

--- dockstore-client/src/main/java/io/dockstore/client/cli/nested/AbstractEntryClient.java
+++ dockstore-client/src/main/java/io/dockstore/client/cli/nested/AbstractEntryClient.java
@@ -1252,8 +1252,15 @@ public abstract class AbstractEntryClient<T> {
      */
     private void processWesCommands(final List<String> args) {
         WesCommandParser wesCommandParser = new WesCommandParser();
+        out(wesCommandParser.jCommander.getCommands().toString());
+//        for (String str : wesCommandParser.jCommander.getCommands().keySet()) {
+//            out(str);
+//        }
+//        for (String str : wesCommandParser.jCommander.getCommands().keySet()) {
+//            out(str);
+//        }
 
-        // JCommander throws a parameter exception for invalid parameters. Catch this an print the error cleanly.
+        // JCommander throws a parameter exception for invalid parameters. Catch this and print the error cleanly.
         try {
             wesCommandParser.jCommander.parse(args.toArray(new String[0]));
         } catch (ParameterException e) {
