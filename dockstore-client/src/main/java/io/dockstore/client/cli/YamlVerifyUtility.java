20aa167f784f3e6668862389cff9e7e5abeb2c99
Flynn Hembroff <85577755+fhembroff@users.noreply.github.com>
2023-04-26 16:12:34 -0400

    Ensuring that Dockstore Yaml Validate Rejects .dockstore.yml files that are missing or have an empty primaryDescriptorPath (#235)
    * Enabled disabled test, and it now works!
    
    * Made validateEntries true in readAsDockstoreYaml12
    
    * Removed unnecessary .toString()s
    
    * Removed unused import
    
    * Removed unneeded line of code
    
    * Did two things
    1. Added tests
    2. Ensured that all of our tools were CWL
    
    * Fixed checkstyle violation
    
    * Changed primaryDescriptorPath of dockstore-client/src/test/resources/YamlVerifyTestDirectory/correct-directory/tool/.dockstore.yml from Dockstore2.wdl to Dockstore2.cwl.
    
    Also had to rename file
    
    * Moved repetitive code in method
    
    * Added Assert.fail to test to ensure that an error is always thrown (as expected)

--- dockstore-client/src/main/java/io/dockstore/client/cli/YamlVerifyUtility.java
+++ dockstore-client/src/main/java/io/dockstore/client/cli/YamlVerifyUtility.java
@@ -71,10 +71,6 @@ public final class YamlVerifyUtility {
     private static List<String> filesExist(List<String> paths, String base) {
         List<String> missingFiles = new ArrayList<>();
         for (String path : paths) {
-            if (path == null) {
-                missingFiles.add("primaryDescriptorPath" + FILE_DOES_NOT_EXIST);
-                continue;
-            }
             Path pathToFile = Paths.get(base, path);
             if (!Files.exists(pathToFile)) {
                 missingFiles.add(pathToFile + FILE_DOES_NOT_EXIST);
@@ -141,19 +137,19 @@ public final class YamlVerifyUtility {
 
         // Verify that the path is a valid directory
         if (!Files.isDirectory(workflowPath)) {
-            throw new ValidateYamlException(INVALID_DOCKSTORE_YML + workflowPath.toString() + FILE_DOES_NOT_EXIST);
+            throw new ValidateYamlException(INVALID_DOCKSTORE_YML + workflowPath + FILE_DOES_NOT_EXIST);
         }
 
         // Verify that .dockstore.yml exists
         Path dockstoreYmlPath = Paths.get(path, DOCKSTOREYML);
         if (!Files.exists(dockstoreYmlPath)) {
-            throw new ValidateYamlException(INVALID_DOCKSTORE_YML + dockstoreYmlPath.toString() + FILE_DOES_NOT_EXIST);
+            throw new ValidateYamlException(INVALID_DOCKSTORE_YML + dockstoreYmlPath + FILE_DOES_NOT_EXIST);
         }
 
         // Verify that .dockstore.yml is non-empty
         File dockstoreYml = new File(dockstoreYmlPath.toString());
         if (dockstoreYml.length() == 0) {
-            throw new ValidateYamlException(INVALID_DOCKSTORE_YML + dockstoreYmlPath.toString() + EMPTY_FILE);
+            throw new ValidateYamlException(INVALID_DOCKSTORE_YML + dockstoreYmlPath + EMPTY_FILE);
         }
 
         // Load dockstoreYml into string
@@ -162,7 +158,7 @@ public final class YamlVerifyUtility {
             dockstoreYmlString = Files.readString(dockstoreYmlPath);
         } catch (IOException ex) {
             // Unlikely to ever catch
-            throw new ValidateYamlException("Error reading " + dockstoreYmlPath.toString() + " :" +  System.lineSeparator() + ex.getMessage());
+            throw new ValidateYamlException("Error reading " + dockstoreYmlPath + " :" +  System.lineSeparator() + ex.getMessage());
         }
 
         // Verify that the Yaml is valid, however does not verify it is valid for dockstore
@@ -171,7 +167,7 @@ public final class YamlVerifyUtility {
             safeYaml.load(Files.readString(dockstoreYmlPath));
             out(dockstoreYmlPath + VALID_YAML_ONLY);
         } catch (Exception ex) {
-            throw new ValidateYamlException(INVALID_DOCKSTORE_YML + dockstoreYmlPath.toString() + INVALID_YAML + ex.getMessage());
+            throw new ValidateYamlException(INVALID_DOCKSTORE_YML + dockstoreYmlPath + INVALID_YAML + ex.getMessage());
         }
 
         // Running validate first, as if readAsDockstoreYaml12 is run first and a parameter is incorrect it is difficult to understand
@@ -179,15 +175,15 @@ public final class YamlVerifyUtility {
         try {
             DockstoreYamlHelper.validateDockstoreYamlProperties(dockstoreYmlString);
         } catch (Exception ex) {
-            throw new ValidateYamlException(INVALID_DOCKSTORE_YML + dockstoreYmlPath.toString() + CONTAINS_ERRORS + ex.getMessage());
+            throw new ValidateYamlException(INVALID_DOCKSTORE_YML + dockstoreYmlPath + CONTAINS_ERRORS + ex.getMessage());
         }
 
         // Validates file dockstoreYaml in general
         DockstoreYaml12 dockstoreYaml12 = null;
         try {
-            dockstoreYaml12 = DockstoreYamlHelper.readAsDockstoreYaml12(dockstoreYmlString);
+            dockstoreYaml12 = DockstoreYamlHelper.readAsDockstoreYaml12(dockstoreYmlString, true);
         } catch (Exception ex) {
-            throw new ValidateYamlException(INVALID_DOCKSTORE_YML + dockstoreYmlPath.toString() + CONTAINS_ERRORS + ex.getMessage());
+            throw new ValidateYamlException(INVALID_DOCKSTORE_YML + dockstoreYmlPath + CONTAINS_ERRORS + ex.getMessage());
         }
 
         if (workflowPath.endsWith(GITHUB_DIRECTORY_NAME)) {
