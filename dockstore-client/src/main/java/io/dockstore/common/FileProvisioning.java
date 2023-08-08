7539c18b09b846837ca9702d6b93cbcaf99773dc
Nayeon Hyun <61166764+hyunnaye@users.noreply.github.com>
2023-08-08 11:07:38 -0400

    DOCK-2323: Log source and destination paths for file provisioning (#252)
    * log source and destination paths
    
    * changed message

--- dockstore-client/src/main/java/io/dockstore/common/FileProvisioning.java
+++ dockstore-client/src/main/java/io/dockstore/common/FileProvisioning.java
@@ -146,6 +146,10 @@ public class FileProvisioning {
         return "true".equalsIgnoreCase(useCache) || "use".equalsIgnoreCase(useCache) || "T".equalsIgnoreCase(useCache);
     }
 
+    private static void printSourceAndDestination(String sourcePath, Path destinationPath) {
+        System.out.println("Saving " + sourcePath  + " to " + destinationPath);
+    }
+
     /*
      * Retrieves plugin name given object class name.
      * Splits outer plugin class name (which extends Plugin abstract class) and inner class name (which extends
@@ -661,6 +665,7 @@ public class FileProvisioning {
 
         @Override
         public boolean downloadFrom(String sourcePath, Path destination) {
+            printSourceAndDestination(sourcePath, destination);
             return FileProvisionUtil.downloadFromVFS2(sourcePath, destination, threads);
         }
 
