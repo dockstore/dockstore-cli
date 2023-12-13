a289f66b1de2bee92a5ae9048c6d1b38208c8652
Denis Yuen <denis.yuen@oicr.on.ca>
2023-12-13 15:55:02 -0500

    remove workaround

--- dockstore-client/src/main/java/io/dockstore/client/cli/nested/DepCommand.java
+++ dockstore-client/src/main/java/io/dockstore/client/cli/nested/DepCommand.java
@@ -62,10 +62,8 @@ public final class DepCommand {
             ApiClient defaultApiClient;
             defaultApiClient = Configuration.getDefaultApiClient();
             MetadataApi metadataApi = new MetadataApi(defaultApiClient);
-            // todo: revert after real release
-            String trimmedVersionD = commandDep.clientVersion.split("-")[0];
             String runnerDependencies = metadataApi
-                    .getRunnerDependencies(trimmedVersionD, commandDep.pythonVersion, commandDep.runner, "text");
+                    .getRunnerDependencies(commandDep.clientVersion, commandDep.pythonVersion, commandDep.runner, "text");
             if (runnerDependencies == null) {
                 errorMessage("Could not get runner dependencies", API_ERROR);
             } else {
