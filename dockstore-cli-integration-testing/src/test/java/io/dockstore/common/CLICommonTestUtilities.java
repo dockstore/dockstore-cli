c4b16eb0df62f271741be04eb4ced1a89ef6a07d
Denis Yuen <denis.yuen@oicr.on.ca>
2024-06-21 10:22:34 -0400

    add migration

--- dockstore-cli-integration-testing/src/test/java/io/dockstore/common/CLICommonTestUtilities.java
+++ dockstore-cli-integration-testing/src/test/java/io/dockstore/common/CLICommonTestUtilities.java
@@ -100,7 +100,7 @@ public final class CLICommonTestUtilities {
 
         List<String> migrationList = Arrays
             .asList("1.3.0.generated", "1.3.1.consistency", "test", "1.4.0", "1.5.0", "test_1.5.0", "1.6.0", "1.7.0",
-                    "1.8.0", "1.9.0", "1.10.0", "1.11.0", "1.12.0", "1.13.0", "1.14.0", "1.15.0");
+                    "1.8.0", "1.9.0", "1.10.0", "1.11.0", "1.12.0", "1.13.0", "1.14.0", "1.15.0", "1.16.0");
         CommonTestUtilities.runMigration(migrationList, application, dropwizardConfigurationFile);
     }
 
