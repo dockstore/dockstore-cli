798b25369f62802688ac263464a2b7a933362825
Denis Yuen <denis.yuen@oicr.on.ca>
2024-08-13 16:35:40 -0400

    syntax

--- scripts/install-tests.sh
+++ scripts/install-tests.sh
@@ -19,6 +19,7 @@ else
     # https://stackoverflow.com/questions/44331836/apt-get-install-tzdata-noninteractive needed by cwltool
     DEBIAN_FRONTEND=noninteractive sudo apt-get -qq --yes --force-yes install tzdata
     sudo apt-get -qq --yes --force-yes install cwltool=3.1.20220224085855-1
+fi
 
 if [ "${TESTING_PROFILE}" = "singularity-tests" ]; then
     # Install singularity from source
